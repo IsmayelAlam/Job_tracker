31c6c437020b1b872158fa5050292d18050f9ac5
Ismayel Alam <ismayelalam0@gmail.com>
2023-11-11 12:48:36 +0600

    create register page

--- src/page/Register.jsx
+++ src/page/Register.jsx
@@ -1,10 +1,60 @@
-import { Link } from "react-router-dom";
+import { Form, Link } from "react-router-dom";
+import styled from "styled-components";
+import Logo from "../components/Logo";
+import FormRow from "../components/FormRow";
+import SubmitBtn from "../components/SubmitBtn";
 
 export default function Register() {
   return (
-    <div>
-      <h1>Register</h1>
-      <Link to="/login">Login</Link>
-    </div>
+    <Wrapper>
+      <Form method="post" className="form">
+        <Logo />
+        <h4>Register</h4>
+        <FormRow type="text" name="name" />
+        <FormRow type="text" name="lastName" labelText="last name" />
+        <FormRow type="text" name="location" />
+        <FormRow type="email" name="email" />
+        <FormRow type="password" name="password" />
+        <SubmitBtn />
+        <p>
+          Already a member?
+          <Link to="/login" className="member-btn">
+            Login
+          </Link>
+        </p>
+      </Form>
+    </Wrapper>
   );
 }
+
+const Wrapper = styled.section`
+  min-height: 100vh;
+  display: grid;
+  align-items: center;
+  .logo {
+    display: block;
+    margin: 0 auto;
+    margin-bottom: 1.38rem;
+  }
+  .form {
+    max-width: 400px;
+    border-top: 5px solid var(--primary-500);
+  }
+  h4 {
+    text-align: center;
+    margin-bottom: 1.38rem;
+  }
+  p {
+    margin-top: 1rem;
+    text-align: center;
+    line-height: 1.5;
+  }
+  .btn {
+    margin-top: 1rem;
+  }
+  .member-btn {
+    color: var(--primary-500);
+    letter-spacing: var(--letter-spacing);
+    margin-left: 0.25rem;
+  }
+`;
