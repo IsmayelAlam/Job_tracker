163019c7045b217af61c46e6d37fe75ceead057c
Ismayel Alam <ismayelalam0@gmail.com>
2023-11-23 20:58:28 +0600

    add pagination function to server

--- server/controller/jobController.js
+++ server/controller/jobController.js
@@ -5,7 +5,7 @@ import mongoose from "mongoose";
 import dayjs from "dayjs";
 
 export const getAllJobs = async (req, res) => {
-  const { search, status, type } = req.query;
+  const { search, status, type, sort } = req.query;
 
   const queryObj = {
     createdBy: req.user.userId,
@@ -19,8 +19,28 @@ export const getAllJobs = async (req, res) => {
   if (status && status !== "all") queryObj.status = status;
   if (type && type !== "all") queryObj.type = type;
 
-  const jobs = await jobModel.find(queryObj);
-  res.status(StatusCodes.OK).json({ jobs, size: jobs.length });
+  const sortOptions = {
+    newest: "-createdAt",
+    oldest: "createdAt",
+    "a-z": "position",
+    "z-a": "-position",
+  };
+  const sortKey = sortOptions[sort];
+
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  const jobs = await jobModel
+    .find(queryObj)
+    .sort(sortKey)
+    .skip(skip)
+    .limit(limit);
+
+  const totalJobs = await jobModel.countDocuments(queryObj);
+  const numOfPages = Math.ceil(totalJobs / limit);
+
+  res.status(StatusCodes.OK).json({ totalJobs, page, numOfPages, jobs });
 };
 
 export const getJob = async (req, res) => {
